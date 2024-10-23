79f9f8a4c4e09055a7576462abf98bba91810c60
agorabuilder <agorabuilder@agora.io>
2024-10-23 09:34:21 +0000

    0.0.2-test

--- Package.swift
+++ Package.swift
@@ -1,21 +1,19 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "AgoraAosl_iOS", // 替换为你的包名
+    name: "AgoraInfra",
     defaultLocalization: "en",
-    platforms: [
-        .iOS(.v14), // 替换为你支持的平台
-        .macOS(.v11)
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(name: "RtcBasic", targets: ["aosl"]),
     ],
     targets: [
         .binaryTarget(
             name: "aosl",
-            url: "https://download.agora.io/sdk/release/aosl.xcframework.zip", // 替换为你的目标 URL
-            checksum: "de7a5fa3ab3cafa527fc5fb4e36894856220dcf7f82a418ed9af0e0f79638416" // 替换为你的目标的校验和
+            url: "https://download.agora.io/swiftpm/AgoraInfra_iOS/0.0.2-test/aosl.xcframework.zip",
+            checksum: "0271b20f04e1ba03174af9d60e541a5f7480facdc109b1dfdb2ef4f966d3e736"
         ),
     ]
 )
-
