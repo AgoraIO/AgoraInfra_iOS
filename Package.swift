c2781d81a1ac78b94fefd53171975acb426b6ac5
agorabuilder <agorabuilder@agora.io>
2024-11-26 02:41:28 +0000

    1.2.13

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "aosl",
-            url: "https://download.agora.io/swiftpm/AgoraInfra_iOS/0.0.17-test/aosl.xcframework.zip",
-            checksum: "a628549610aa1dd316888597a7b559107f251fdc70f120cd74dd969709580644"
+            url: "https://download.agora.io/swiftpm/AgoraInfra_iOS/1.2.13/aosl.xcframework.zip",
+            checksum: "657691b309d8b4c525a0d31c9c847cc06fe54b4e11fbb2c58c0741bdfda4af19"
         ),
     ]
 )
