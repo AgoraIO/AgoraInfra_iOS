6c3d2b9673b356585de0d5d65a794d6b6b738d12
Ren Jingui <renjingui@agora.io>
2026-09-04 10:33:29 +0000

    0.0.0-r.0

--- Package.swift
+++ Package.swift
@@ -1,19 +1,19 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "AgoraInfra_iOS",
     defaultLocalization: "en",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v9), .visionOS(.v1)],
     products: [
         .library(name: "AgoraInfra_iOS", targets: ["aosl"]),
     ],
     targets: [
         .binaryTarget(
             name: "aosl",
-            url: "https://download.agora.io/swiftpm/AgoraInfra_iOS/1.3.4/aosl.xcframework.zip",
-            checksum: "1c020086acf45881c62e5fc100a6187a899b25e1eb464db405e8589fcccfc3e8"
+            url: "https://download.agora.io/swiftpm/AgoraInfra_iOS/0.0.0-r.0/._aosl.xcframework.zip",
+            checksum: "d59d489090a0158a6ec21b61a952a3bc6c431884262b2020658b767c26958e11"
         ),
     ]
 )
